4441c31ac633a28fa34856230123658c2f4fb7f5
Javi <javsteratii@gmail.com>
2019-10-25 00:22:35 -0400

    UPDATED WORKING WITH ORDERS

--- lib/models/command_line_interface.rb
+++ lib/models/command_line_interface.rb
@@ -1,5 +1,6 @@
 require "pry"
 class CommandLineInterface
+
     
         def greet
             font = TTY::Font.new(:straight)
@@ -17,6 +18,20 @@ class CommandLineInterface
             end 
         end
 
+        def orders(user)
+        prompt = TTY::Prompt.new
+           prior_orders = Order.find_by(user_id: user)
+           puts " "
+           puts "Here are all previous orders affiliated with your account:"
+           puts prior_orders
+           puts " "
+                prompt.select("What would you like to do now?") do |menu|
+                    puts " "
+                    menu.choice "Return to Account Page", -> { log_in }
+                    menu.choice "Exit", -> { exit } 
+                end
+        end 
+
         def users_reprompted
             puts "Welcome To Bird!"
             puts "Please try again!"
@@ -27,7 +42,6 @@ class CommandLineInterface
             puts "Thanks! See ya later!"
         end 
 
-        
         def balance(userb)
             prompt = TTY::Prompt.new
             blob = userb
@@ -38,27 +52,59 @@ class CommandLineInterface
             puts "Increase your balance by charging more Birds!"
                 prompt.select("Recieve $5 for every Bird picked up!") do |menu|
                     menu.choice "CHARGE NOW", -> { birds(blob) }
-                    menu.choice "EXIT", -> { exit }
+                    menu.choice "EXIT", -> { exit }    
                 end       
         end 
 
-        def pickfive(user)
-            puts " "
-            puts "Thank you #{user.username}, We've added $25 to your balance!"
-            user.balance + 25.0
-            puts "Your new balance is #{user.balance}"
-            
-        end
-
-        def pickone(user)
+        def pickone(user, bird)
+            prompt = TTY::Prompt.new
+            user.update(:balance => user.balance + 5.0)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
             puts " "
             puts "Thank you #{user.username}, We've added $5 to your balance!"
+            puts  "Your new balance is #{user.balance}"
+                prompt.select("What would you like to do now?") do |menu|
+                    puts " "
+                    menu.choice "Return to Account Page", -> { log_in }
+                    menu.choice "Exit", -> { exit }
+                end    
         end 
 
-        def pickmax(user, amount)
+        def pickthree(user, bird)
+            prompt = TTY::Prompt.new
+            user.update(:balance => user.balance + 15.0) 
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
             puts " "
-            puts "Thank you #{user.username}, You picked up #{amount} birds and We've added $#{amount * 5} to your balance!"
+            puts "Thank you #{user.username}, We've added $25 to your balance!"
+            puts "Your new balance is #{user.balance}"
+                prompt.select("What would you like to do now?") do |menu|
+                    puts " "
+                    menu.choice "Return to Account Page", -> { log_in }
+                    menu.choice "Exit", -> { exit }
+                end
         end
+
+        def pickseven (user, bird)
+            prompt = TTY::Prompt.new
+            user.update(:balance => user.balance + 35.0)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            Order.create(scooter_id: bird.id, user_id: user.id, earnings: 5)
+            puts " "
+            puts "Thank you #{user.username}, We've added $25 to your balance!"
+            puts "Your new balance is #{user.balance}"
+                prompt.select("What would you like to do now?") do |menu|
+                    puts " "
+                    menu.choice "Return to Account Page", -> { log_in }
+                    menu.choice "Exit", -> { exit }
+                end 
+        end 
 
         def birds(current_user)
             prompt = TTY::Prompt.new
@@ -66,12 +112,13 @@ class CommandLineInterface
                 city = current_user.location
                 available_scooters = Bird.all.select { |bird| bird.location == city }
                 chargable_scooters = available_scooters.select {|bird| bird.battery_percentage <= 75.0}
+                bird = chargable_scooters.sample
                     puts "   "
                     puts "There are #{chargable_scooters.count} available scooters to charge in your area!"
                     prompt.select("How many would you like to pick up and charge?") do |menu|
-                        menu.choice "PICK UP 1 SCOOTER", -> { pickone(blanky) }
-                        menu.choice "PICK UP 5 SCOOTERS", -> { pickfive(blanky) }
-                        menu.choice "PICK UP MAX SCOOTERS", -> { pickmax(blanky,chargable_scooters.count) }
+                        menu.choice "PICK UP 1 SCOOTER", -> { pickone(blanky, bird) }
+                        menu.choice "PICK UP 3 SCOOTERS", -> { pickthree(blanky, bird) }
+                        menu.choice "PICK UP 7 SCOOTERS", -> { pickseven(blanky,chargable_scooters.count) }
                     end
         end 
        
@@ -91,7 +138,7 @@ class CommandLineInterface
                     key(:password).ask("Password:", required: true)
                 end
                 
-                User.create(name: charger[:name], balance: 0, location: charger[:location], username: charger[:username], password: charger[:password])
+                User.create(name: charger[:name], balance: 0.0, location: charger[:location], username: charger[:username], password: charger[:password])
                     puts "Welcome #{charger[:username]}! Thanks for signing up!"
                     log_in
         end 
@@ -101,7 +148,7 @@ class CommandLineInterface
                 user = prompt.collect do
                     puts "Please log in."
                     puts "========="
-                    key(:username).ask("Please enter your username now!")
+                    key(:username).ask("Please enter your username:")
                     puts "Thank you!"
                     puts " "
                     key(:password).ask("You may now your password:")
@@ -123,6 +170,7 @@ class CommandLineInterface
                             prompt.select("What would you like to do?") do |menu|
                                 puts "     "
                                 menu.choice "View my Balance", -> { balance(find_result)}
+                                menu.choice "View Previous Orders", -> { orders(find_result) }
                                 menu.choice "Charge Birds", -> { birds(find_result) }
                                 menu.choice "Exit", -> { exit }
                                 end 
